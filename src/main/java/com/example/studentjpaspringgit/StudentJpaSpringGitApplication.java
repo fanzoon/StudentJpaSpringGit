3453b6500b791b105d25549edd50323475de932c
ALIAKSEI RAIKO <alexeyraiko644@gmail.com>
2023-06-05 22:52:59 +0300

    Initial commit

---
+++ src/main/java/com/example/studentjpaspringgit/StudentJpaSpringGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentjpaspringgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentJpaSpringGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentJpaSpringGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studentjpaspringgit/StudentJpaSpringGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentjpaspringgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentJpaSpringGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
